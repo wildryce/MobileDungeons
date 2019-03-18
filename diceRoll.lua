a48ed868d7eda5571bed77f73576ab110bfc1c4c
toostroke <48604168+toostroke@users.noreply.github.com>
2019-03-18 15:35:06 -0700

    Update diceRoll.lua
    Added rollDie function to diceRoll.lua

--- diceRoll.lua
+++ diceRoll.lua
@@ -1,4 +1,4 @@
-local M = {}
+--[[local M = {}
 
 local function testFunction()
 	print("rolled 1D4")
@@ -10,7 +10,41 @@ local function secondTestFunction()
 	print("rolled 1D8")
 	print( M.secondTestFunction )
 end
-M.secondTestFunction = secondTestFunction	
+M.secondTestFunction = secondTestFunction
 
+
+return M--]]
+
+local M = {}
+
+local function rollDie(number,die)
+	if number == 1 then
+		local rollResult = math.random(1,die)
+		return rollResult
+	elseif number == 2 then
+		local rollResult = (math.Random(1,die)) + (math.Random(1,die))
+		return rollResult
+	elseif number == 3 then
+		local rollResult = (math.Random(1,die)) + (math.Random(1,die)+ (math.Random(1,die))
+		return rollResult
+	elseif number == 4 then
+		local rollResult = (math.Random(1,die)) + (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die))
+		return rollResult
+	elseif number == 5 then
+		local rollResult = (math.Random(1,die)) + (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die))
+		return rollResult
+	elseif number == 6 then
+		local rollResult = (math.Random(1,die)) + (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die))
+		return rollResult
+	elseif number == 7 then
+		local rollResult = (math.Random(1,die)) + (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die))
+		return rollResult
+	elseif number == 8 then
+		local rollResult = (math.Random(1,die)) + (math.Random(1,die+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die)+ (math.Random(1,die))
+		return rollResult
+	end
+end
+
+M.rollDie = rollDie
 
 return M
